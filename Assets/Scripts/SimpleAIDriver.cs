23d65a3958fdadfdd7a3c936d460b8630a36cb19
BuildTools <unconfigured@null.spigotmc.org>
2024-06-14 15:14:08 -0600

    AI-Dev Modified AI behavior so steering was less jittery. Also added start race functionaility with the R key

--- Assets/Scripts/SimpleAIDriver.cs
+++ Assets/Scripts/SimpleAIDriver.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class SimpleAIDriver : MonoBehaviour
 {
 
+    // Cole Donovan
+    // Basic AI Script that handles point to point driving
+
     [SerializeField] private Transform[] targetPositionTransforms;
 
     private CarController carController;
     private Vector3 currentTargetPosition;
     private int targetNumber;
+
+    public bool isRaceActive = false;
 
     private void Awake()
     {
@@ -19,10 +24,26 @@ public class SimpleAIDriver : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            isRaceActive = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isRaceActive)
+        {
+            ControlAI();
+        }
+    }
+
+    private void ControlAI()
+    {
         SetTargetPosition(targetPositionTransforms[targetNumber].position);
-        float bufferDistance = 7f;
+        float bufferDistance = 8.5f;
         float distance = Vector3.Distance(transform.position, currentTargetPosition);
-        
+
 
         Vector2 newInputs = new Vector2(0f, 1f);
 
@@ -37,22 +58,29 @@ public class SimpleAIDriver : MonoBehaviour
             }
 
             float turnAngle = Vector3.SignedAngle(transform.forward, directionToMove, Vector3.up);
-
-            if (turnAngle > 0)
+            Debug.Log(turnAngle);
+            if (turnAngle > 5)
             {
                 newInputs.x = 1f;
+                newInputs.y = turnAngle / 180f;
+            }
+            else if (turnAngle < -5)
+            {
+                newInputs.x = -1f;
+                newInputs.y = turnAngle / -180f;
             }
             else
             {
-                newInputs.x = -1f;
+                newInputs.x = turnAngle / 180f;
             }
         }
         else
         {
-            if(targetNumber == targetPositionTransforms.Length - 1)
+            if (targetNumber == targetPositionTransforms.Length - 1)
             {
                 newInputs.x = 0f;
-                newInputs.y = 1f;
+                newInputs.y = 0f;
+                isRaceActive = false;
             }
             else
             {
