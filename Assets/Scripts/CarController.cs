302ca64bc70e98a659ec1a407d787dc73a5db9ba
Damon Estrada <damonestrada1@gmail.com>
2024-06-29 14:42:15 -0700

    Randomly selects a clip from an audio array to play a sound when a collision with another car happened.

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CarController : MonoBehaviour
 {
@@ -62,6 +64,9 @@ public class CarController : MonoBehaviour
     public float Strength => strength;
     public float Damping => damping;
 
+    public AudioSource carCollisionAudioSource;
+    public AudioClip[] carCollisionSounds;
+
     void Start()
     {
         Debug.Log($"CarController Start - Initial topSpeed: {topSpeed}");
@@ -73,6 +78,11 @@ public class CarController : MonoBehaviour
         }
 
         startHeight = transform.position.y;
+    }
+
+    private void Awake()
+    {
+        carCollisionAudioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -242,5 +252,14 @@ public class CarController : MonoBehaviour
             }
         }
         activePowerups.Clear();
+    }
+
+    private void OnCollisionEnter(Collision otherEntity)
+    {
+        if (otherEntity.gameObject.CompareTag("Car"))
+        {
+            carCollisionAudioSource.clip = carCollisionSounds[Random.Range(0, carCollisionSounds.Length)];
+            carCollisionAudioSource.PlayOneShot(carCollisionAudioSource.clip);
+        }
     }
 }
