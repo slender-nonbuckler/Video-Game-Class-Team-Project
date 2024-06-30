28d7e14f0bd400e1df88a6716c1434a69025bafa
LuiMoiPer <luimoiper@outlook.com>
2024-06-29 18:18:29 -0700

    WIP: not much here.

---
+++ Assets/Scripts/RaceManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+/**
+ * In charge of an individual racetrack, its setup progress and completion.
+ *
+ * Responsibilities:
+ * - Providing start locations for the racers
+ * - Keeping track of individual racer progress
+ * - Knowing when the race is over
+ * - Providing info on race events like overtakes, or last laps.
+ */
+public class RaceManager : MonoBehaviour {
+    
+}
