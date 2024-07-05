9caabed294e6890baefab47c0a62d7b7abd50798
LuiMoiPer <luimoiper@outlook.com>
2024-07-04 22:18:27 -0700

    Make it so gameobjects are only destroyed when required.

--- Assets/Scripts/TrackMarking/AiFlowCircle.cs
+++ Assets/Scripts/TrackMarking/AiFlowCircle.cs
@@ -13,22 +13,26 @@ public class AiFlowCircle : MonoBehaviour {
     private float _radius;
     [SerializeField] private float angleOffset = 0f;
     private float _angleOffset;
-    
 
     private float pow = 0.5f;
     private float turnFraction = 1.618033988749f;
 
-    private void Start() {
-        SyncValues();
+    private void Awake() {
         MakePoints();
     }
 
     void Update() {
-        if (isChanged()) {
+        if (!isChanged()) {
+            return;
+        }
+
+        if (isNumOfPointsChanged()) {
             DestroyPoints();
             MakePoints();
-            SyncValues();
         }
+
+        SyncPoints();
+        SyncValues();
     }
 
     private bool isChanged() {
@@ -43,19 +47,30 @@ public class AiFlowCircle : MonoBehaviour {
         return false;
     }
 
+    private bool isNumOfPointsChanged() {
+        return numOfPoints != _numOfPoints;
+    }
+
     private void MakePoints() {
         for (int i = 0; i < numOfPoints; i++) {
-            float dst = Mathf.Pow(i / (numOfPoints - 1f), pow) * radius;
+            GameObject point = new GameObject($"{name}:{i}", typeof(AiWaypoint));
+            point.tag = AiWaypoint.TAG;
+            point.transform.parent = transform;
+        }
+    }
+
+    private void SyncPoints() {
+        for (int i = 0; i < transform.childCount; i++) {
+            float dst = Mathf.Pow(i / (transform.childCount - 1f), pow) * radius;
             float angle = 2 * Mathf.PI * turnFraction * i;
             float angleDeg = angle * Mathf.Rad2Deg;
 
             float z = dst * Mathf.Cos(angle);
             float x = dst * Mathf.Sin(angle);
 
-            GameObject point = new GameObject($"{name}:{i}", typeof(AiWaypoint));
-            point.transform.parent = transform;
-            point.transform.localPosition = new Vector3(x, 0f, z);
-            point.transform.localRotation = UnityEngine.Quaternion.Euler(0f, angleDeg + angleOffset, 0f);
+            Transform point = transform.GetChild(i);
+            point.localPosition = new Vector3(x, 0f, z);
+            point.localRotation = UnityEngine.Quaternion.Euler(0f, angleDeg + angleOffset, 0f);
         }
     }
 
