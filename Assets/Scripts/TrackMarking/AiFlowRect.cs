999db782d7c32ff0229aa3250c15b119b492687d
LuiMoiPer <luimoiper@outlook.com>
2024-07-06 16:50:48 -0700

    Update AiFlowRect to have margins that point towards the center

--- Assets/Scripts/TrackMarking/AiFlowRect.cs
+++ Assets/Scripts/TrackMarking/AiFlowRect.cs
@@ -2,16 +2,21 @@ using UnityEngine;
 
 [ExecuteInEditMode]
 public class AiFlowRect : MonoBehaviour {
-    [SerializeField] private float width = 1f;
+    [SerializeField] private float width = 100f;
     private float _width;
-    [SerializeField] private float height = 1f;
+    [SerializeField] private float length = 100f;
     private float _height;
-    [SerializeField] private float pointsPerWidthUnit = 0.5f;
+    [SerializeField] private float pointsPerWidthUnit = 0.1f;
     private float _pointsPerWidthUnit;
-    [SerializeField] private float pointsPerHeightUnit = 0.5f;
+    [SerializeField] private float pointsPerLenghtUnit = 0.1f;
     private float _pointsPerHeightUnit;
-    [SerializeField] private float angleOffset = 0f;
+    [SerializeField] private float angleOffset;
     private float _angleOffset;
+
+    [SerializeField] private float marginWidth = 10f;
+    private float _marginWidth;
+    [SerializeField] private float marginAngleOffset;
+    private float _marginAngleOffset;
 
     void Update() {
         if (!isChanged()) {
@@ -30,64 +35,82 @@ public class AiFlowRect : MonoBehaviour {
     private bool isNumOfPointsChanged() {
         return (
             width != _width
-            || height != _height
+            || length != _height
+            || marginWidth != _marginWidth
             || pointsPerWidthUnit != _pointsPerWidthUnit
-            || pointsPerHeightUnit != _pointsPerHeightUnit
+            || pointsPerLenghtUnit != _pointsPerHeightUnit
         );
     }
 
     private bool isChanged() {
         return (
             width != _width
-            || height != _height
+            || length != _height
             || pointsPerWidthUnit != _pointsPerWidthUnit
-            || pointsPerHeightUnit != _pointsPerHeightUnit
+            || pointsPerLenghtUnit != _pointsPerHeightUnit
             || angleOffset != _angleOffset
+            || marginWidth != _marginWidth
+            || marginAngleOffset != _marginAngleOffset
         );
     }
-    
+
     private void MakePoints() {
-        int numOfWidthPoints = Mathf.FloorToInt(width * pointsPerWidthUnit); 
-        int numOfHeightPoints = Mathf.FloorToInt(height * pointsPerHeightUnit);
+        float totalWidth = width + marginWidth * 2;
+        int numOfWidthPoints = Mathf.FloorToInt(totalWidth * pointsPerWidthUnit);
+        int numOfHeightPoints = Mathf.FloorToInt(length * pointsPerLenghtUnit);
         int numOfPoints = numOfWidthPoints * numOfHeightPoints;
         for (int i = 0; i < numOfPoints; i++) {
             GameObject point = new GameObject($"{name}:{i}", typeof(AiWaypoint));
             point.transform.parent = transform;
         }
     }
-    
+
     private void DestroyPoints() {
         while (transform.childCount > 0) {
             DestroyImmediate(transform.GetChild(0).gameObject);
         }
     }
-    
+
     private void SyncValues() {
         _width = width;
-        _height = height;
+        _height = length;
         _pointsPerWidthUnit = pointsPerWidthUnit;
-        _pointsPerHeightUnit = pointsPerHeightUnit;
+        _pointsPerHeightUnit = pointsPerLenghtUnit;
         _angleOffset = angleOffset;
+        _marginWidth = marginWidth;
+        _marginAngleOffset = marginAngleOffset;
     }
-    
+
     private void SyncPoints() {
-        int numOfWidthPoints = Mathf.FloorToInt(width * pointsPerWidthUnit); 
-        int numOfHeightPoints = Mathf.FloorToInt(height * pointsPerHeightUnit);
+        float totalWidth = width + marginWidth * 2;
+        int numOfWidthPoints = Mathf.FloorToInt(totalWidth * pointsPerWidthUnit);
+        int numOfHeightPoints = Mathf.FloorToInt(length * pointsPerLenghtUnit);
 
-        float widthSpacing = width / numOfWidthPoints;
-        float heightSpacing = height / numOfHeightPoints;
+        float widthSpacing = totalWidth / numOfWidthPoints;
+        float heightSpacing = length / numOfHeightPoints;
 
-        float widthOffset = widthSpacing / 2f - width / 2f;
-        float heightOffset = widthSpacing / 2f - height / 2f;
-        
+        float widthOffset = widthSpacing / 2f - totalWidth / 2f;
+        // float heightOffset = widthSpacing / 2f - height / 2f;
+
         for (int i = 0; i < transform.childCount; i++) {
-
             float x = i % numOfWidthPoints * widthSpacing + widthOffset;
-            float z = i / numOfWidthPoints * heightSpacing + heightOffset;
+            float z = i / numOfWidthPoints * heightSpacing;
 
             Transform point = transform.GetChild(i);
             point.localPosition = new Vector3(x, 0f, z);
-            point.localRotation = Quaternion.Euler(0f, angleOffset, 0f);
+            point.localRotation = Quaternion.Euler(0f, GetRotation(point) + angleOffset, 0f);
         }
+    }
+
+    private float GetRotation(Transform point) {
+        float xPosition = point.localPosition.x;
+        if (Mathf.Abs(xPosition) < width / 2f) {
+            return 0f;
+        }
+
+        float distFromCore = Mathf.Abs(xPosition) - width / 2f;
+        float percentInMargin = distFromCore / marginWidth;
+
+        return Mathf.LerpAngle(0f, marginAngleOffset, percentInMargin) * Mathf.Sign(xPosition);
     }
 }
