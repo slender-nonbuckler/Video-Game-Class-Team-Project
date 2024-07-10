715dd61162e5eb970cdbc2e6b68f3ddcd2462ef0
LuiMoiPer <luimoiper@outlook.com>
2024-07-10 01:09:25 -0700

    Updated scene and falling monolith animation.

--- Assets/Scripts/TriggerableObject.cs
+++ Assets/Scripts/TriggerableObject.cs
@@ -1,30 +1,15 @@
 using UnityEngine;
 
-public class TriggerableObject : MonoBehaviour
-{
-    public GameObject triggerableGameObject;
-
-    private bool isObjectFallen = false;
+public class TriggerableObject : MonoBehaviour {
+    private bool isObjectFallen;
     [SerializeField] private Animator _animator;
 
     private static readonly int CauseObjectToFall = Animator.StringToHash("CauseObjectToFall");
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
+    private void OnTriggerEnter(Collider other) {
         var carController = other.attachedRigidbody.gameObject.GetComponent<CarController>();
 
-        if (carController && !isObjectFallen)
-        {
+        if (carController && !isObjectFallen) {
             _animator.SetBool(CauseObjectToFall, true);
             isObjectFallen = true;
         }
