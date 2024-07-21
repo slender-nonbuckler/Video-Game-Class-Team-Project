38a06ac946172611e58d254c5649cbc668a8b71f
LuiMoiPer <luimoiper@outlook.com>
2024-07-20 18:19:52 -0700

    Add colliders for new layer to vehicle prefabs

--- Assets/Scripts/SpeedBooster.cs
+++ Assets/Scripts/SpeedBooster.cs
@@ -4,6 +4,11 @@ public class SpeedBooster : MonoBehaviour {
     [SerializeField] private float boostForce;
 
     private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer != LayerMask.NameToLayer("TriggerColliders")) {
+            return;
+        }
+        
+        Debug.Log($"Boosting: {other.gameObject}");
         other.attachedRigidbody.AddForce(transform.forward * boostForce, ForceMode.VelocityChange);
     }
 }
